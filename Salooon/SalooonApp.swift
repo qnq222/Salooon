a833980662662d527a751f42996a5d470ea001ad
Ayman A. AbuMutair <qnq222@gmail.com>
2024-11-04 09:11:33 +0200

    Initial Commit

---
+++ Salooon/SalooonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalooonApp.swift
+//  Salooon
+//
+//  Created by Ayman AbuMutair on 04/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SalooonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
